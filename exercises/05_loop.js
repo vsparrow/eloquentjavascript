6946b114767a9efd816820c447b046fe82986f7d
vsparrow <khan@vsparrow.com>
2018-11-05 00:39:53 -0500

    5: Add loop

---
+++ exercises/05_loop.js
@@ -0,0 +1,30 @@
+// Write a higher-order function loop
+// something like a for loop statement.
+// It takes
+//   a value,
+//   a test function,
+//   an update function,
+//   a body function.
+// Each iteration,
+//   it first runs the test function on the current loop value
+//   and stops if that returns false.
+//   Then it calls the body function, giving it the current value.
+//   Finally, it calls the update function
+//     to create a new value and starts from the beginning.
+
+let loop = (startValue, testF, updateF, bodyF) => {
+  while(testF(startValue)){
+    bodyF(startValue)
+    startValue = updateF(startValue)
+  }
+}
+
+let loopUsingFor = (value, test, update, body) => {
+  for(let i = value; test(i); i = update(i)) { body(i)}
+}
+
+loop(3, n => n > 0, n => n - 1, console.log);
+loopUsingFor(3, n => n > 0, n => n - 1, console.log);
+// → 3
+// → 2
+// → 1
